fd85a9acaf48a13ee7cf1a637b22cda1836e6797
Luke Petrocochino <luke.petro@coodash.com>
2021-08-17 17:04:04 +0100

    adding more styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { AgGridColumn, AgGridReact } from "ag-grid-react";
 
-import "ag-grid-community/dist/styles/ag-grid.css";
-import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import "./App.scss";
 
 const App = () => {
   const [rowData, setRowData] = useState([]);
@@ -14,27 +13,40 @@ const App = () => {
       .then((rowData) => setRowData(rowData));
   }, []);
 
+  // style={{ height: 345, width: 600 }}
+
   return (
-    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
-      <AgGridReact ref={gridRef} rowData={rowData} rowSelection="multiple">
-        <AgGridColumn
-          field="name"
-          sortable={true}
-          filter={true}
-          checkboxSelection={true}
-        ></AgGridColumn>
-        <AgGridColumn
-          field="subscribedToChannel"
-          sortable={true}
-          filter={true}
-        ></AgGridColumn>
-        <AgGridColumn
-          field="subscribeData"
-          sortable={true}
-          filter={true}
-        ></AgGridColumn>
-      </AgGridReact>
-    </div>
+    <>
+      <div
+        className="ag-theme-alpine"
+        style={{ height: "100%", width: "100%" }}
+      >
+        <AgGridReact
+          ref={gridRef}
+          rowData={rowData}
+          rowSelection="multiple"
+          suppressHorizontalScroll={false}
+        >
+          <AgGridColumn
+            field="name"
+            sortable={true}
+            filter={true}
+            checkboxSelection={true}
+          ></AgGridColumn>
+          <AgGridColumn
+            field="subscribedToChannel"
+            sortable={true}
+            filter={true}
+          ></AgGridColumn>
+          <AgGridColumn
+            field="subscribeData"
+            sortable={true}
+            filter={true}
+            flex={1}
+          ></AgGridColumn>
+        </AgGridReact>
+      </div>
+    </>
   );
 };
 
